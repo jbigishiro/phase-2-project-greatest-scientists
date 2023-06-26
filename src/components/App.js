5f9089ce9d36f7ff370a97a33130e7899e4dcc08
Justin Bigishiro <justin.rvd@gmail.com>
2023-06-26 05:14:36 -0500

    add components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import scientists from "../data/db.json"
+import Navbar from "./Navbar"
+import Scientists from "./Scientists"
+import Favorites from "./Favorites"
+import MostRated from "./MostRated"
 
 function App() {
   return (
     <div>
-    wWelcome to my app
+    Welcome to my app
+    <Navbar/>
+    <Scientists/>
+    <Favorites/>
+    <MostRated/>
     </div>
   );
 }
---
+++ src/components/Scientists.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Scientists() {
+  return (
+    <div>Scientists</div>
+  )
+}
+
+export default Scientists
