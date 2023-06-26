3b9602f2492f5cc65c647433587a04cff691b405
Justin Bigishiro <justin.rvd@gmail.com>
2023-06-26 15:57:49 -0500

    adds fetch code on scientists.js file

--- src/components/Scientists.js
+++ src/components/Scientists.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 function Scientists() {
+  const [people, setPeople] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3001/scientists")
+      .then((r) => r.json())
+      .then((scientists) => setPeople(scientists));
+  }, []);
+
+  const scientistList = people.map((person) => (
+    <li key={person.id}>
+      <h3>{person.name}</h3>
+      <img scr={person.image} alt={person.name} />
+    </li>
+  ));
+
   return (
-    <div>Scientists</div>
-  )
+    <div>
+      <h2>Scientists</h2>
+
+      <ul>{scientistList} </ul>
+    </div>
+  );
 }
 
-export default Scientists
+export default Scientists;
