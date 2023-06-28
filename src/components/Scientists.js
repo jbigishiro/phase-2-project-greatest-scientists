7d2fceacb3e6795d511d6913f765083257d17c78
Justin Bigishiro <justin.rvd@gmail.com>
2023-06-28 11:10:57 -0500

    adds hide, show details codes on Scientists.js file

--- src/components/Scientists.js
+++ src/components/Scientists.js
@@ -1,27 +1,36 @@
 import React, { useState } from "react";
 
 function Scientists({ people }) {
-  const [showDetails, setShowDetails] = useState(false);
+  const [showDetails, setShowDetails] = useState({});
 
-  const handleToggle = () => {
-    setShowDetails(!showDetails);
+  const handleToggle = (personId) => {
+    setShowDetails((prevState) => ({
+      ...prevState,
+      [personId]: !prevState[personId]
+    }));
   };
 
   const scientistList = people.map((person) => (
     <li key={person.id}>
-      <h3>{person.name} ({person.year})</h3>
+      <h3>
+        {person.name} ({person.year})
+      </h3>
 
       <img src={person.image} alt={person.name} />
 
       <div>
-        <button onClick={handleToggle}>
-          {showDetails ? "Hide Details" : "Show Details"}
+        <button onClick={()=> handleToggle(person.id)}>
+          {showDetails[person.id] ? "Hide Details" : "Show Details"}
         </button>
-        {showDetails && (
-          <div >
+        {showDetails[person.id]  && (
+          <div>
             <h3>Field: {person.field}</h3>
             <h3>Achievements</h3>
-            <ul style={{backgroundColor: "white"}} > {person.achievements.map(achievement=><li key={achievement}>{achievement}</li>)} </ul>
+            <ul style={{ backgroundColor: "white" }}>
+              {person.achievements.map((achievement) => (
+                <li key={achievement}>{achievement}</li>
+              ))}
+            </ul>
           </div>
         )}
       </div>
