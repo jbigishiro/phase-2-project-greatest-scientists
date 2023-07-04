b2ebce7f067b8a4d32a733054a79378cf9b9932a
Justin Bigishiro <justin.rvd@gmail.com>
2023-07-04 06:21:04 -0500

    adds login data on db.json file

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <div>This is the login form </div>
-  )
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h2>User Login</h2>
+        <label>
+          Username <span>*</span>
+          <input type="text" value={username} onChange={e=>setUsername(e.target.value) } />
+        </label>{" "}
+        <br />
+        <label>
+          Password <span>*</span>
+          <input type="password" value={password} onChange={e=>setPassword(e.target.value) }/>
+        </label>{" "}
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
